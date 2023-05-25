066b66a8b6a73129990d5349e9b0d38a5acc103c
nguyen huu duc <duccccnguyen@gmail.com>
2023-05-25 20:15:31 +0700

    @duc/2

--- AppStore/DAL/InvoiceDetailDAL.cs
+++ AppStore/DAL/InvoiceDetailDAL.cs
@@ -31,16 +31,58 @@ namespace DAL
         }
 
         // add InvoiceDetail
-        public void addOrUpdateInvoiceDetail(InvoiceDetail invoiceDetail)
+        public bool addInvoiceDetail(InvoiceDetail ct)
         {
-            db.InvoiceDetails.AddOrUpdate(invoiceDetail);
-            db.SaveChanges();
+            try
+            {
+                db.InvoiceDetails.AddOrUpdate(ct);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         // trả về danh sách InvoiceDetail theo invoiceID
-        public dynamic getListInvoiceDetailByInvoiceID(int id)
+        public List<InvoiceDetail> getListInvoiceDetailByInvoiceID(int id)
         {
-            return db.InvoiceDetails.Where(p=>p.InvoiceID == id ).Select(p=> new {p.Product.ProductName}).ToList();
+            return db.InvoiceDetails.Where(p => p.InvoiceID == id).ToList();
+        }
+        public InvoiceDetail getInvoiceDetail(int invoiceId)
+        {
+            InvoiceDetail d = (from id in db.InvoiceDetails where id.InvoiceID == invoiceId select id).FirstOrDefault();
+            return d;
+        }
+        public InvoiceDetail getInvoiceDetailByProduct(int productId)
+        {
+            InvoiceDetail d = (from id in db.InvoiceDetails where id.ProductID == productId select id).FirstOrDefault();
+            return d;
+        }
+        public Object getInvoiceDetailByProduct1(int productId)
+        {
+            Object d = (from id in db.InvoiceDetails
+                        where id.ProductID == productId
+                        select new
+                        {
+                            products = id.Product,
+                            invoice = id.Invoice,
+                        }).FirstOrDefault();
+            return d;
+        }
+
+        public List<InvoiceDetail> GetInvoiceDetails()
+        {
+            // var kq = from dl in db.InvoiceDetails select dl
+            return db.InvoiceDetails.ToList();
+
+        }
+
+        public void Save(InvoiceDetail i)
+        {
+            db.InvoiceDetails.AddOrUpdate(i);
+            db.SaveChanges();
         }
     }
 }
